b759d3100e8c18134d2071cc6a291f8732c57175
Szymon Tworkowski <44476995+syzymon@users.noreply.github.com>
2023-06-21 13:26:53 +0200

    Fix off-by-one in example generation
    `list.insert` inserts before a given index, if `random_line` is 1, then we insert before the first element, which results in a malformed prompt: the instruction line is before "Testing Long Context", which is slightly off (it can be easily reproduced if n=2 is used).

--- little_retrieval_test.py
+++ little_retrieval_test.py
@@ -29,7 +29,7 @@ def generate_and_modify_text_file(filename, n, shuffle_flag, B):
     lines.extend([f"line {i}: REGISTER_CONTENT is <{random.randint(1, 50000)}>\n" for i in line_numbers])
     random_line = random.randint(1, n)
     # add the EXECUTE instruction in random line of the text
-    lines.insert(random_line - 1, f"[EXECUTE THIS]: Go to line {random_line} and report only REGISTER_CONTENT, without any context or additional text, just the number, then EXIT\n")
+    lines.insert(random_line, f"[EXECUTE THIS]: Go to line {random_line} and report only REGISTER_CONTENT, without any context or additional text, just the number, then EXIT\n")
     with open(filename, "w") as f:
         f.writelines(lines)
 
